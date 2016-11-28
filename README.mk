1f65eadf79135d95018ec6b77bfec500bfdfb3cd
CapsHil <pierre.raby.pro@gmail.com>
2016-11-28 23:36:52 +0100

    Add README

---
+++ README.mk
@@ -0,0 +1,7 @@
+# Star Wars Clicker
+
+### Features
+
+You can click on the Empire logo to generate a Galactic Credit (GC).
+When you have enought GC, buy some Stormtrooper, TIE Fighter or Star Destroyer...
+You can also unlock some bonus, to increase your GC generation per click
